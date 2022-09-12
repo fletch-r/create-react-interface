be0c5a019f37df4f8273da5da40c9f1872bec8fb
0xATHERIS <ath3ris@proton.me>
2022-09-12 23:52:25 +0100

    Update Button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button
+
 export interface ButtonProps {
+  autofocus: boolean;
+  disabled: boolean;
+  form: string;
+  formaction: string;
+  // Default is application/x-www-form-urlencoded
+  formenctype:
+    | "application/x-www-form-urlencoded"
+    | "multipart/form-data"
+    | "text/plain";
+  formmethod: "post" | "get";
+  formnovalidate: boolean;
+  // _self is default
+  formtarget: "_self" | "_blank" | "_parent" | "_top";
+  name: string;
+  // submit is default
+  type: "submit" | "reset" | "button";
+  value: string;
   label: string;
 }
+
+// Add functionallity for Icon Buttons
+// If its an icon button there has to be hidden text like
+// Add to favourites
+// So screen readers know to read the name Add to favourites and not the SVG
+
+// Minimum size of 44x44 pixels
 
 const Button = (props: ButtonProps) => {
   return <button>{props.label}</button>;
