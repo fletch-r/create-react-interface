28d5e2f5301376b3cf9488396379a79d27790839
0xATHERIS <ath3ris@proton.me>
2023-01-12 01:13:19 +0000

    deleted comments

--- bin/utils/cloneRepo.js
+++ bin/utils/cloneRepo.js
@@ -27,32 +27,3 @@ module.exports = (projectName) => {
 	const cloneSsh = tryGitClone(ssh, projectName);
 	return cloneSsh.status === 0;
 };
-
-// const gitCheckoutCommand = `git clone --depth 1 https://github.com/0xATHERIS/create-react-interface ${repoName}`;
-// const installDepsCommand = `cd ${repoName} && npm install`;
-
-// console.log(`Cloning the repository with name: ${repoName}`);
-
-// const checkedOut = runCommand(gitCheckoutCommand);
-// if (!checkedOut) {
-// 	console.error(`Failed to clone the repository with name: ${repoName}`);
-// 	process.exit(1);
-// }
-
-// console.log(
-// 	`Installing dependencies for the repository with name: ${repoName}`
-// );
-// const installedDeps = runCommand(installDepsCommand);
-// if (!installedDeps) {
-// 	console.error(
-// 		`Failed to install dependencies for the repository with name: ${repoName}`
-// 	);
-// 	process.exit(1);
-// }
-
-// console.log(
-// 	`Successfully cloned and installed the repository with name: ${repoName}`
-// );
-// console.log(`Run the following commands to get started:`);
-// console.log(`cd ${repoName}`);
-// console.log(`npm start`);
