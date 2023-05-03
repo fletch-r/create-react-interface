df1ba29d4cd8b5fcbf09ffff95eacecac64f94c5
0xATHERIS <ath3ris@proton.me>
2023-05-03 01:19:19 +0100

    feat: :sparkles:  - installs create-react-interface template folder

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,8 +1,48 @@
 #!/usr/bin/env node
-var execSync = require('child_process').execSync;
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var child_process_1 = require("child_process");
+var fse = require("fs-extra");
+var path = require("path");
 var runCommand = function (command) {
     try {
-        execSync("".concat(command), { stdio: 'inherit' });
+        (0, child_process_1.execSync)("".concat(command), { stdio: 'inherit' });
     }
     catch (error) {
         console.error("Failed to execute command: ".concat(command), error);
@@ -10,26 +50,48 @@ var runCommand = function (command) {
     }
     return true;
 };
-var repoName = process.argv[2];
-if (!repoName) {
-    console.error('No Name Provided');
-    process.exit(1);
+function main() {
+    return __awaiter(this, void 0, void 0, function () {
+        var repoName, git_commands, checkedOut, srcDir, tmpProjectDir, projectDir, installDepsCommand, installedDeps;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    repoName = process.argv[2];
+                    if (!repoName) {
+                        console.error('No Name Provided');
+                        process.exit(1);
+                    }
+                    git_commands = "git clone --depth 1 --no-checkout https://github.com/0xATHERIS/create-react-interface.git ".concat(repoName, "\n\n    cd ").concat(repoName, "\n\n    git sparse-checkout init --cone\n\n    git sparse-checkout set template\n\n    git checkout @");
+                    console.log("Creating the repository with name: ".concat(repoName));
+                    return [4 /*yield*/, runCommand(git_commands)];
+                case 1:
+                    checkedOut = _a.sent();
+                    if (!checkedOut) {
+                        console.error("Failed to create the repository with name: ".concat(repoName));
+                        process.exit(1);
+                    }
+                    srcDir = path.join("./".concat(repoName), "template");
+                    tmpProjectDir = path.join('./', "".concat(repoName, "_tmp"));
+                    fse.mkdirSync(tmpProjectDir);
+                    fse.copySync(srcDir, tmpProjectDir, { overwrite: true });
+                    projectDir = path.join('./', repoName);
+                    fse.emptyDirSync(projectDir);
+                    fse.copySync(tmpProjectDir, projectDir, { overwrite: true });
+                    fse.rmSync(tmpProjectDir, { recursive: true, force: true });
+                    installDepsCommand = "cd ".concat(repoName, " && npm install");
+                    console.log("Installing dependencies for the repository with name: ".concat(repoName));
+                    installedDeps = runCommand(installDepsCommand);
+                    if (!installedDeps) {
+                        console.error("Failed to install dependencies for the repository with name: ".concat(repoName));
+                        process.exit(1);
+                    }
+                    console.log("Successfully created and installed the repository with name: ".concat(repoName));
+                    console.log("Run the following commands to get started:");
+                    console.log("cd ".concat(repoName));
+                    console.log("npm start");
+                    return [2 /*return*/];
+            }
+        });
+    });
 }
-var gitCheckoutCommand = "git clone https://github.com/0xATHERIS/create-react-interface.git ".concat(repoName);
-var installDepsCommand = "cd ".concat(repoName, " && npm install");
-console.log("Cloning the repository with name: ".concat(repoName));
-var checkedOut = runCommand(gitCheckoutCommand);
-if (!checkedOut) {
-    console.error("Failed to clone the repository with name: ".concat(repoName));
-    process.exit(1);
-}
-console.log("Installing dependencies for the repository with name: ".concat(repoName));
-var installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) {
-    console.error("Failed to install dependencies for the repository with name: ".concat(repoName));
-    process.exit(1);
-}
-console.log("Successfully cloned and installed the repository with name: ".concat(repoName));
-console.log("Run the following commands to get started:");
-console.log("cd ".concat(repoName));
-console.log("npm start");
+main();
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process');
+import { execSync } from "child_process";
 
-const runCommand = command => {
+const fse = require("fs-extra");
+const path = require("path");
+
+const runCommand = (command: string) => {
 	try {
 		execSync(`${command}`, { stdio: 'inherit' });
 	} catch (error) {
@@ -12,32 +15,53 @@ const runCommand = command => {
 	return true;
 };
 
-const repoName = process.argv[2];
+async function main() {
+    const repoName = process.argv[2];
 
-if (!repoName) {
-    console.error('No Name Provided');
-    process.exit(1);
+    if (!repoName) {
+        console.error('No Name Provided');
+        process.exit(1);
+    }
+
+    const git_commands = `git clone --depth 1 --no-checkout https://github.com/0xATHERIS/create-react-interface.git ${repoName}
+
+    cd ${repoName}
+
+    git sparse-checkout init --cone
+
+    git sparse-checkout set template
+
+    git checkout @`;
+
+    console.log(`Creating the repository with name: ${repoName}`);
+
+    const checkedOut = await runCommand(git_commands);
+    if (!checkedOut) {
+        console.error(`Failed to create the repository with name: ${repoName}`);
+        process.exit(1);
+    }
+
+    const srcDir = path.join(`./${repoName}`, "template");
+    const tmpProjectDir = path.join('./', `${repoName}_tmp`);
+    fse.mkdirSync(tmpProjectDir);
+    fse.copySync(srcDir, tmpProjectDir, { overwrite: true });
+    const projectDir = path.join('./', repoName);
+    fse.emptyDirSync(projectDir);
+    fse.copySync(tmpProjectDir, projectDir, { overwrite: true });
+    fse.rmSync(tmpProjectDir, { recursive: true, force: true });
+
+    const installDepsCommand = `cd ${repoName} && npm install`;
+    console.log(`Installing dependencies for the repository with name: ${repoName}`);
+    const installedDeps = runCommand(installDepsCommand);
+    if (!installedDeps) {
+        console.error(`Failed to install dependencies for the repository with name: ${repoName}`);
+        process.exit(1);
+    }
+    
+    console.log(`Successfully created and installed the repository with name: ${repoName}`);
+    console.log(`Run the following commands to get started:`);
+    console.log(`cd ${repoName}`);
+    console.log(`npm start`);
 }
 
-const gitCheckoutCommand = `git clone https://github.com/0xATHERIS/create-react-interface.git ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
-
-console.log(`Cloning the repository with name: ${repoName}`);
-
-const checkedOut = runCommand(gitCheckoutCommand);
-if (!checkedOut) {
-	console.error(`Failed to clone the repository with name: ${repoName}`);
-	process.exit(1);
-}
-
-console.log(`Installing dependencies for the repository with name: ${repoName}`);
-const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) {
-	console.error(`Failed to install dependencies for the repository with name: ${repoName}`);
-	process.exit(1);
-}
-
-console.log(`Successfully cloned and installed the repository with name: ${repoName}`);
-console.log(`Run the following commands to get started:`);
-console.log(`cd ${repoName}`);
-console.log(`npm start`);
+main();
